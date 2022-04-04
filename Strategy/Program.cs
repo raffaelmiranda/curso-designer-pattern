903c35e642950ef79aa73755c88bbcea4525345d
Rafael Miranda <rafael.miranda@solutis.com.br>
2022-04-04 08:59:42 -0300

    strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,16 @@
+﻿using Strategy;
+
+PedidoEletronicos pedido = new PedidoEletronicos();
+pedido.Valor(100);
+
+//Frete Comun
+IFrete freteComum = new FreteComum();
+pedido.Frete(freteComum);
+Console.WriteLine($"Frete Comun: R$ {pedido.CalcularFrete()}");
+
+//Frete Expresso
+IFrete freteExpresso = new FreteExpresso();
+pedido.Frete(freteExpresso);
+Console.WriteLine($"Frete Expresso: R$ {pedido.CalcularFrete()}");
+
+
